b6f84f7350bcb013971efa7eefd2f77c1ef2ccdc
ResulShafili <qiyasogluzefer@gmail.com>
2026-04-12 16:41:08 +0400

    Add type annotations to concat function

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ResulShafili concat function"""
 
+
 def concat(str1: str, str2: str) -> str:
     """Concatenates two strings and returns the result"""
     return str1 + str2
