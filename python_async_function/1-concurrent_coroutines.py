ce271954b6caa87a96dce8cab8e3d6172568b77b
ResulShafili <qiyasogluzefer@gmail.com>
2026-04-12 22:09:21 +0400

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,7 +3,8 @@
 
 import asyncio
 import bisect
-from 0-basic_async_syntax import wait_random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
