9f1ed39d5267a42bfd3f5e9de6fa0d9ac06d16c5
ResulShafili <qiyasogluzefer@gmail.com>
2026-04-12 17:53:11 +0400

    Update type hinting and docstring in sum_mixed_list
    Refactor sum_mixed_list to use Union for type hinting and improve docstring.

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""ResulShafili"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """this function sums ints and floats of this list"""
+    return sum(mxd_lst)
