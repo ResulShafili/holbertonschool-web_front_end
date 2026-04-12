875190a631646e3c4a0a71060f61685b9c1b37d3
ResulShafili <qiyasogluzefer@gmail.com>
2026-04-12 17:00:39 +0400

    Implement sum_list function to sum a list of floats
    Fix the function name to 'sum' and improve docstring.

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""ResulShafili list function"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """this function sums all variables of list"""
+    return Sum(input_list)
